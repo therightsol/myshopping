48f40cced8691ccc75bdefab206fc529337a60a0
assad47 <akasadkhan80@gmail.com>
2016-10-08 03:50:52 -0700

    Reset Password page created

--- 01-htmltoPHP files by asad/Reset.php
+++ 01-htmltoPHP files by asad/Reset.php
@@ -3,6 +3,7 @@
 <?php include 'inc/header.inc.php'; ?>
 <?php include 'inc/menu.inc.php'; ?>
 
+
 <div id="option-panel">
     <div id="option-panel-wrapper">
         <div id="option-panel-btn"></div>
@@ -248,174 +249,36 @@
         <div class="container">
             <ul class="breadcrumb">
                 <li><a href="index.html">Home</a></li>
-                <li class="active">Portfolio 4 Column</li>
+                <li class="active">Reset Password</li>
             </ul>
         </div>
     </div>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <header class="content-title"><h1 class="title">Products</h1>
+                <header class="content-title"><h1 class="title">Reset Password</h1>
 
-                    <p class="title-desc">Look at all works of out authors.</p></header>
-                <ul id="portfolio-filter" class="clearfix">
-                    <li><a href="portfolio-4.html#" class="active" data-filter="*">All</a></li>
-                    <li><a href="portfolio-4.html#" data-filter=".photography">Photography</a></li>
-                    <li><a href="portfolio-4.html#" data-filter=".fashion">Fashion</a></li>
-                    <li><a href="portfolio-4.html#" data-filter=".video">Video</a></li>
-                    <li><a href="portfolio-4.html#" data-filter=".design">Design</a></li>
-                    <li><a href="portfolio-4.html#" data-filter=".wordpress">Wordpress</a></li>
-                </ul>
-                <div class="row portfolio-item-container" data-maxcolumn="4" data-layoutmode="fitRows">
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item photography">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio4.jpg" alt="Portfolio item 4">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio4.jpg" title="Portfolio item 4"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>16</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
+                    <div class="md-margin"></div>
+                </header>
+                <div class="row">
 
-                        <p><a href="portfolio-4.html#">Photography</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item wordpress">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio11.jpg" alt="Portfolio item 11">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio11.jpg" title="Portfolio item 11"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>9</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
+                    <div class="col-md-6 col-sm-6 col-xs-12"><h2>Reset your password</h2>
 
-                        <p><a href="portfolio-4.html#">Design</a>, <a href="portfolio-4.html#">Wordpress</a></p>
-                    </div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item video">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio1.jpg" alt="Portfolio item 1">
-                            <figcaption><a href="http://www.youtube.com/watch?v=vEkwlqsdbyk"
-                                           title="Portfolio Video Example" data-rel="prettyPhoto[portfolio]"
-                                           class="zoom-button"></a> <a href="single-portfolio.html"
-                                                                       class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>14</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
+                        <p>Enter your email, username or phone to get a new password.</p>
 
-                        <p><a href="portfolio-4.html#">Video</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item photography">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio8.jpg" alt="Portfolio item 8">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio8.jpg" title="Portfolio item 8"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>8</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
+                        <div class="xs-margin"></div>
+                        <form id="reset-form" method="get" action="reset.html#">
+                            <div class="input-group"><span class="input-group-addon"><span
+                                        class="input-icon input-icon-user"></span><span
+                                        class="input-text">Details&#42;</span></span> <input type="text" required
+                                                                                             class="form-control input-lg"
+                                                                                             placeholder="Email, Username or Phone">
+                            </div>
 
-                        <p><a href="portfolio-4.html#">Photography</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item fashion photography">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio13.jpg" alt="Portfolio item 13">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio13.jpg" title="Portfolio item 13"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>5</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Fashion</a>, <a href="portfolio-4.html#">Photography</a></p>
-                    </div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item fashion video">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio14.jpg" alt="Portfolio item 14">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio14.jpg" title="Portfolio item 14"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>12</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Fashion</a>, <a href="portfolio-4.html#">Video</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item fashion photography">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio10.jpg" alt="Portfolio item 10">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio10.jpg" title="Portfolio item 10"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>22</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Fashion</a>, <a href="portfolio-4.html#">Photography</a></p>
-                    </div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item design video">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio3.jpg" alt="Portfolio item 3">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio3.jpg" title="Portfolio item 3"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>18</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Design</a>, <a href="portfolio-4.html#">Video</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item design wordpress fashion">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio2.jpg" alt="Portfolio item 2">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio2.jpg" title="Portfolio item 2"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>12</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Design</a>, <a href="portfolio-4.html#">Wordpress</a>, <a
-                                href="portfolio-4.html#">Fashion</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item design">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio15.jpg" alt="Portfolio item 15">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio15.jpg" title="Portfolio item 15"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>7</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Design</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item design video">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio16.jpg" alt="Portfolio item 16">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio16.jpg" title="Portfolio item 16"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>41</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Design</a>, <a href="portfolio-4.html#">Video</a></p></div>
-                    <div class="col-md-3 col-sm-4 col-xs-6 portfolio-item fashion photography">
-                        <figure><img src="<?php echo $root; ?>assets/images/portfolio/classic/portfolio17.jpg" alt="Portfolio item 17">
-                            <figcaption><a href="<?php echo $root; ?>assets/images/portfolio/classic/portfolio17.jpg" title="Portfolio item 17"
-                                           data-rel="prettyPhoto[portfolio]" class="zoom-button"></a> <a
-                                    href="single-portfolio.html" class="link-button"></a> <a
-                                    href="portfolio-4.html#" class="like-button"><i
-                                        class="fa fa-heart"></i><span>33</span></a></figcaption>
-                        </figure>
-                        <h2><a href="portfolio-4.html#">Lorem ipsum dolor</a></h2>
-
-                        <p><a href="portfolio-4.html#">Fashion</a>, <a href="portfolio-4.html#">Photography</a></p>
-                    </div>
-                </div>
-                <div class="pagination-container clear-border clearfix">
-                    <div class="pull-right">
-                        <ul class="pagination">
-                            <li class="active"><a href="portfolio-4.html#">1</a></li>
-                            <li><a href="portfolio-4.html#">2</a></li>
-                            <li><a href="portfolio-4.html#">3</a></li>
-                            <li><a href="portfolio-4.html#">4</a></li>
-                            <li><a href="portfolio-4.html#">5</a></li>
-                            <li><a href="portfolio-4.html#"><i class="fa fa-angle-right"></i></a></li>
-                        </ul>
+                            <span class="help-block text-right">Remembered your password? <a href="login.html#">Go ahead and login.</a></span>
+                            <button class="btn btn-custom-2">Reset My Password</button>
+                        </form>
+                        <div class="sm-margin"></div>
                     </div>
                 </div>
             </div>
