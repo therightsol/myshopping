5f5e0b5a085803d76378db648aaca4e13867611a
ob0981 <obaidemail@gmail.com>
2016-10-08 19:09:43 +0500

    Comapre-product and category list
    created controllers for compare-product and category list pages

--- application/controllers/Category_list.php
+++ application/controllers/Category_list.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Category_list extends CI_Controller {
     public function index(){
-        $this->load->view('category-list');
+        $this->load->view('Category-list');
     }
 }
