abf17863468055d426bb48f302944415b5a7a019
assad47 <akasadkhan80@gmail.com>
2016-11-02 14:39:41 -0700

    dashboard functions created

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,4 +5,25 @@ class Dashboard extends CI_Controller {
     public function index(){
         $this->load->view('dashboard');
     }
+
+    public function login(){
+        $this->load->view('dashboardlogin');
+    }
+
+    public function logout(){
+        $this->load->view('dashboardlogout');
+    }
+
+    public function settings(){
+        $this->load->view('settings');
+    }
+
+    public function add_product(){
+        $this->load->view('add_product');
+    }
+
+    public function view_all_products(){
+        $this->load->view('view_all_products');
+    }
+
 }
