a0a3bd6c9fe714ff89a4a9a9c94651fa0c149dd5
assad47 <akasadkhan80@gmail.com>
2016-11-12 03:08:05 -0800

    minor changes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,6 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
     public function index(){
+
         $this->load->view('home');
+    }
+
+    public function view_products(){
+
+        $this->load->model('product');
+
+        $tableName = $this->product->getrecord('title');
+
+        $result = get_all_Record_withTable($tableName);
+
+        if($result){
+
+            echo 'This is Product view';
+
+        }else{
+
+            echo 'sorry there is some errors';
+
+        }
+
     }
 }
